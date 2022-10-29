836cd3f49e72403865a3089fa0451b756f2fcc52
Vadim Gorbachev <vadim.gorbachev.main@yandex.ru>
2022-10-29 14:18:49 +0300

    refactored file structure

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@ use actix_web::{web, App, HttpResponse, HttpServer};
 use actix_web::dev::Server;
 use std::net::TcpListener;
 
+pub mod configuration;
+pub mod routes;
+pub mod startup;
+
 #[derive(serde::Deserialize)]
 struct FormData {
     email: String,
---
+++ src/routes/mod.rs
@@ -0,0 +1,5 @@
+mod health_check;
+mod subcriptions;
+
+pub use health_check::*;
+pub use subcriptions::*;
