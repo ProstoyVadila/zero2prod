0878014dad2f428b5304924975138529b9b2f724
Vadim Gorbachev <vadim.gorbachev.main@yandex.ru>
2022-10-29 11:02:51 +0300

    added serde for form models, subcr handler and tests, script for db

---
+++ scripts/init_db.sh
@@ -0,0 +1,18 @@
+
+set -x
+set -eo pipefail
+
+# Check if a custom params have been set, otherwise default ones
+DB_USER=${POSTGRES_USER:=postgres}
+DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
+DB_NAME="${POSTGRES_DB:=newsletter}"
+DB_PORT="${POSTGRES_PORT:=5432}"
+
+docker run \
+    -e POSTGRES_USER=${DB_USER} \
+    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+    -e POSTGRES_DB=${DB_NAME} \
+    -p "${DB_PORT}":5432 \
+    -d postgres \
+    postgres -N 1000
+# Increased max connections for tests
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,17 @@ use actix_web::{web, App, HttpResponse, HttpServer};
 use actix_web::dev::Server;
 use std::net::TcpListener;
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String
+}
+
 async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+async fn subcribe() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
@@ -10,6 +20,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::get().to(subcribe))
         })
         .listen(listener)?
         .run();
