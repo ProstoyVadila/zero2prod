dab459e909f509fe86f01cb3211c85575fb906d8
Vadim Gorbachev <vadim.gorbachev.main@yandex.ru>
2022-10-30 17:47:11 +0300

    testing logging

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -13,6 +13,15 @@ pub async fn subcribe(
     form: web::Form<FormData>,
     pg_pool: web::Data<PgPool>
 ) -> HttpResponse {
+
+    let request_id = Uuid::new_v4();
+    log::info!(
+        "request_id: {} Adding '{}' '{}' as a new subscriber.",
+        request_id,
+        form.name,
+        form.email
+        );
+    log::info!("Saing new subscriber details in the database");
     match sqlx::query!(
             r#"
             insert into subscriptions(id, email, name, subscribed_at)
@@ -26,9 +35,15 @@ pub async fn subcribe(
         .execute(pg_pool.get_ref())
         .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!(
+                "request_id: {} New subscriber details have been saved",
+                request_id
+            );
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            log::error!("request_id: {} Failed to execute query: {:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
